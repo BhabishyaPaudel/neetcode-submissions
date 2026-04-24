c4cf0be09d1332ab326379d8604436638d71d4f5
Bhabishya Paudel <154507428+BhabishyaPaudel@users.noreply.github.com>
2026-04-24 20:48:52 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            if(mp.contains(nums[i])) return true;
+            else{
+                mp.insert({nums[i],1});
+                
+            }
+        }
+        return false;
+        
+        
+    }
+};
